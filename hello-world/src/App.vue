9f79e7081c5601b5a459642eb6be61c01b6c5739
yobo-16 <robert.anibal.ledezma@gmail.com>
2022-08-27 13:57:06 +0200

    Lab done

---
+++ hello-world/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <AboutUs></AboutUs>
+    <ToDoItem></ToDoItem>
+  </div>
+</template>
+
+<script>
+import AboutUs from "./components/AboutUs.vue";
+import ToDoItem from "./components/ToDoItem.vue";
+export default {
+  name: "App",
+  components: {
+    ToDoItem,
+    AboutUs,
+  },
+};
+</script>
+
+<style></style>
